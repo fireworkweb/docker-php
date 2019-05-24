d35c0110455509ad222aa1b5aede8a6fbbbc644c
Daniel Polito <danielbpolito@gmail.com>
2019-05-24 15:16:04 -0300

    Adding wkhtmltopdf images

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
 docker build -t fireworkweb/app:7.1-alpine 7.1-alpine
+docker build -t fireworkweb/app:7.1-alpine-wkhtmltopdf 7.1-alpine-wkhtmltopdf
 docker build -t fireworkweb/app:7.2-alpine 7.2-alpine
+docker build -t fireworkweb/app:7.2-alpine-wkhtmltopdf 7.2-alpine-wkhtmltopdf
 docker build -t fireworkweb/app:7.3-alpine 7.3-alpine
+docker build -t fireworkweb/app:7.3-alpine-wkhtmltopdf 7.3-alpine-wkhtmltopdf
