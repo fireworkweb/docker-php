ab2fb667f80f923c7f2b0a40fbbdcfdcc0dcf644
Daniel Polito <danielbpolito@gmail.com>
2020-02-17 15:08:48 -0300

    Fix Workflow

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-docker build -t fireworkweb/php:7.1-alpine 7.1
-docker build -t fireworkweb/php:7.1-prod 7.1-prod
-docker build -t fireworkweb/php:7.2 7.2
-docker build -t fireworkweb/php:7.2-prod 7.2-prod
-docker build -t fireworkweb/php:7.3 7.3
-docker build -t fireworkweb/php:7.3-prod 7.3-prod
-docker build -t fireworkweb/php:7.4 7.4
-docker build -t fireworkweb/php:7.4-prod 7.4-prod
+docker build --pull -t fireworkweb/php:7.1-alpine 7.1
+docker build --pull -t fireworkweb/php:7.1-prod 7.1-prod
+docker build --pull -t fireworkweb/php:7.2 7.2
+docker build --pull -t fireworkweb/php:7.2-prod 7.2-prod
+docker build --pull -t fireworkweb/php:7.3 7.3
+docker build --pull -t fireworkweb/php:7.3-prod 7.3-prod
+docker build --pull -t fireworkweb/php:7.4 7.4
+docker build --pull -t fireworkweb/php:7.4-prod 7.4-prod
